72aa25d0eebf9dcda0882a9f6997e027f4edb635
i-preetaulakh <preetaulakh078@outlook.com>
2024-10-29 09:47:49 -0400

    Edited child theme

--- themes/teerp-child/functions.php
+++ themes/teerp-child/functions.php
@@ -8,7 +8,7 @@
  * @since Teerp-Child 1.0
  */
 
-function child_enqueue_scripts() {
+function teerp_enqueue_scripts() {
     wp_enqueue_style( 'child-style', get_stylesheet_uri());
 }
-add_action( 'wp_enqueue_scripts', 'child_enqueue_scripts');
+add_action( 'wp_enqueue_scripts', 'teerp_enqueue_scripts');
