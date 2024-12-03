7a55f5a3f3e8105bc9dd8dbafc401888f3a1ffe7
i-preetaulakh <preetaulakh078@outlook.com>
2024-12-03 03:56:43 -0500

    Changed the Designs

--- themes/teerp-child/functions.php
+++ themes/teerp-child/functions.php
@@ -12,3 +12,10 @@ function teerp_enqueue_scripts() {
     wp_enqueue_style( 'child-style', get_stylesheet_uri());
 }
 add_action( 'wp_enqueue_scripts', 'teerp_enqueue_scripts');
+
+function enqueue_logged_in_styles() {
+    if (is_user_logged_in()) {
+        wp_enqueue_style('logged-in-css', get_stylesheet_directory_uri() . '/loggedin-style.css');
+    }
+}
+add_action('wp_enqueue_scripts', 'enqueue_logged_in_styles');
